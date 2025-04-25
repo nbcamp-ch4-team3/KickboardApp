79d8785087523c22f56b3fa6fbc61c1857a19253
LeeSuHyeon <tngus0673@naver.com>
2025-04-25 20:50:03 +0900

    feat: KickboardCoreData 구현

---
+++ KickboardApp/KickboardApp/DataLayer/CoreData/Base/CoreDataStorage.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataStorage.swift
+//  KickboardApp
+//
+//  Created by 이수현 on 4/25/25.
+//
+
+import CoreData
+
+final class CoreDataStorage {
+    static let shared = CoreDataStorage()
+
+    private init() { }
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreData")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
---
+++ KickboardApp/KickboardApp/DataLayer/CoreData/KickboardCoreData.swift
@@ -0,0 +1,87 @@
+//
+//  KickboardCoreData.swift
+//  KickboardApp
+//
+//  Created by 이수현 on 4/25/25.
+//
+
+import CoreData
+
+protocol KickboardCoreDataProtocol {
+    func saveData(with data: Kickboard) throws
+    func readAllData() throws -> [Kickboard]
+    func updateLocation(id: UUID, latitude: Double, longitude: Double) throws
+    func deleteAllData() throws
+}
+
+final class KickboardCoreData: KickboardCoreDataProtocol {
+    private let viewContext: NSManagedObjectContext
+
+    init() {
+        self.viewContext = CoreDataStorage.shared.persistentContainer.viewContext
+    }
+
+    func saveData(with data: Kickboard) throws {
+        let object = KickboardEntity(context: viewContext)
+        object.id = data.id
+        object.type = data.type.rawValue
+        object.battery = Int64(data.battery)
+        object.isAvailable = data.isAvailable
+        object.latitude = data.latitude
+        object.longitude = data.longitude
+
+        do {
+            try viewContext.save()
+        } catch {
+            throw CoreDataError.saveError(error)
+        }
+    }
+
+    //TODO: latitude, longitude, 지도의 범위를 받아서 필터링
+    func readAllData() throws -> [Kickboard] {
+        do {
+            return try viewContext.fetch(KickboardEntity.fetchRequest())
+                .map {
+                    Kickboard(
+                        id: $0.id ?? UUID(),
+                        latitude: $0.latitude,
+                        longitude: $0.longitude,
+                        battery: Int($0.battery),
+                        isAvailable: $0.isAvailable,
+                        typeString: $0.type ?? KickboardType.typeA.rawValue
+                    )
+                }
+        } catch {
+            throw CoreDataError.readError(error)
+        }
+    }
+
+    func updateLocation(id: UUID, latitude: Double, longitude: Double) throws {
+        let fetchRequest = KickboardEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", id as CVarArg)
+
+        do {
+            let result = try viewContext.fetch(fetchRequest)
+            result.forEach {
+                $0.latitude = latitude
+                $0.longitude = longitude
+            }
+            try viewContext.save()
+        } catch {
+            throw CoreDataError.updateError(error)
+        }
+    }
+
+}
+
+extension KickboardCoreData {
+    func deleteAllData() throws {
+        do {
+            let result = try viewContext.fetch(KickboardEntity.fetchRequest())
+            result.forEach { viewContext.delete($0) }
+            try viewContext.save()
+        } catch {
+            throw CoreDataError.deleteError(error)
+        }
+    }
+}
