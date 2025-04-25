bc69b2da4ddac822099893b12dca0ada66c909fd
ChoiAnYong <dksdyd78@naver.com>
2025-04-25 15:03:24 +0900

    add: project 세팅

---
+++ KickboardApp/KickboardApp/PresentationLayer/Feature/Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KickboardApp
+//
+//  Created by 최안용 on 4/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
