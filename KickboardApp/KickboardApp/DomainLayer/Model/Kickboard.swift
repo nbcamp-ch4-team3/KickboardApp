26dad9d09dba27812cce2a3dffaf75aed6100ab7
LeeSuHyeon <tngus0673@naver.com>
2025-04-28 21:48:14 +0900

    refactor/#6: type -> Brand로 변경

--- KickboardApp/KickboardApp/DomainLayer/Model/Kickboard.swift
+++ KickboardApp/KickboardApp/DomainLayer/Model/Kickboard.swift
@@ -13,7 +13,7 @@ struct Kickboard {
     var longitude: Double
     var battery: Int
     var isAvailable: Bool
-    let type: KickboardType
+    let brand: Brand
 
     //TODO: 위, 경도 변경 / 배터리 닳기 / 사용여부 변경
 
@@ -24,59 +24,67 @@ struct Kickboard {
         longitude: Double,
         battery: Int,
         isAvailable: Bool,
-        type: KickboardType
+        brand: Brand
     ) {
         self.id = id
         self.latitude = latitude
         self.longitude = longitude
         self.battery = battery
         self.isAvailable = isAvailable
-        self.type = type
+        self.brand = brand
     }
 
     // CoreData Entity를 Model로 바꿀 때 사용 (CoreData는 type의 rawValue로 저장됨)
-    init(
-        id: UUID,
-        latitude: Double,
-        longitude: Double,
-        battery: Int,
-        isAvailable: Bool,
-        typeString: String
-    ) {
-        let type = KickboardType.allCases.first(where: { $0.rawValue == typeString }) ?? .typeA
-        self.init(
-            id: id,
-            latitude: latitude,
-            longitude: longitude,
-            battery: battery,
-            isAvailable: isAvailable,
-            type: type
-        )
-    }
-
+//    init(
+//        id: UUID,
+//        latitude: Double,
+//        longitude: Double,
+//        battery: Int,
+//        isAvailable: Bool,
+//        brand
+//    ) {
+//        let type = KickboardType.allCases.first(where: { $0.rawValue == typeString }) ?? .kick
+//        self.init(
+//            id: id,
+//            latitude: latitude,
+//            longitude: longitude,
+//            battery: battery,
+//            isAvailable: isAvailable,
+//            type: type
+//        )
+//    }
 }
 
-enum KickboardType: String, CaseIterable {
-    case typeA // 임시
-    case typeB
-
-    // 배터리당 갈 수 있는 거리 (주행 가능 거리 계산용)
-    var distancePerBatteryUnit: Double {
-        switch self {
-        case .typeA:
-            12
-        case .typeB:
-            10
-        }
-    }
-
-    // 분당 가격
-    var pricePerMinute: Int {
-        switch  self {
-        case .typeA:
-            100
-        case .typeB:
-            200
-        }
-    }
-}
+//enum KickboardType: String, CaseIterable {
+//    case kick
+//    case ssingSsing
+//    case beam
+//
+//    var brandName: String {
+//        self.rawValue
+//    }
+//
+//    // 배터리당 갈 수 있는 거리 (주행 가능 거리 계산용)
+//    var distancePerBatteryUnit: Double {
+//        switch self {
+//        case .kick:
+//            0.5
+//        case .ssingSsing:
+//            2.1
+//        case .beam:
+//            1.2
+//        }
+//    }
+//
+//    // 분당 가격
+//    var pricePerMinute: Int {
+//        switch self {
+//        case .kick:
+//            300
+//        case .ssingSsing:
+//            600
+//        case .beam:
+//            400
+//        }
+//    }
+//}
