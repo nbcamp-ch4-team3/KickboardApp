b78c4632a019bece10b31e9a54bb4365eaf2b9c6
SongKyuSeob <sks5075@naver.com>
2025-04-29 01:09:39 +0900

    feat: HomeVC와 HomeView 분리 및 기초 검색 바 구현

--- KickboardApp/KickboardApp/PresentationLayer/Home/ViewController/HomeViewController.swift
+++ KickboardApp/KickboardApp/PresentationLayer/Home/ViewController/HomeViewController.swift
@@ -10,22 +10,25 @@ import NMapsMap
 import CoreLocation
 
 class HomeViewController: UIViewController {
+    private let homeView = HomeView()
     let locationManager = CLLocationManager()
-    let mapView = NMFMapView()
+
+    override func loadView() {
+        view = homeView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(mapView)
-        mapView.frame = view.frame
-
         locationManager.delegate = self
-
+        homeView.delegate = self
     }
 
-    private func askLocationPermission() {
 
+}
 
+private extension HomeViewController {
+    func askLocationPermission() {
         guard CLLocationManager.locationServicesEnabled() else {
             showRequestLocationServiceAlert()
             return
@@ -36,7 +39,7 @@ class HomeViewController: UIViewController {
         checkUserCurrentLocationAuthorization(authorizationStatus)
     }
 
-    private func checkUserCurrentLocationAuthorization(_ status: CLAuthorizationStatus) {
+    func checkUserCurrentLocationAuthorization(_ status: CLAuthorizationStatus) {
         switch status {
         case .notDetermined:
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -52,9 +55,10 @@ class HomeViewController: UIViewController {
         }
     }
 
-    private func showRequestLocationServiceAlert() {
+    func showRequestLocationServiceAlert() {
         let requestLocationServiceAlert = UIAlertController(title: "위치 정보 이용", message: "위치 서비스를 사용할 수 없습니다.\n디바이스의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
         let moveToSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+            // TODO: - 앱 설정 맨 첫 페이지로 이동하는 방법 찾아야함. 앱 scheme,
             if let appSetting = URL(string: UIApplication.openSettingsURLString) {
                 print(appSetting)
                 Task {
@@ -75,18 +79,18 @@ class HomeViewController: UIViewController {
         }
     }
 
-    private func updateToCurrentPosition() {
+    func updateToCurrentPosition() {
         let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(
             lat: locationManager.location?.coordinate.latitude ?? 0,
             lng: locationManager.location?.coordinate.longitude ?? 0
         ))
-
         cameraUpdate.animation = .easeIn
         DispatchQueue.main.async {
-            self.mapView.moveCamera(cameraUpdate)
+            self.homeView.moveCamera(to: cameraUpdate)
         }
     }
 }
+
 
 // TODO: - 앱 실행 시 Location 권한 요청 -> 온보딩 구현 -> 로그인 후
 
@@ -102,5 +106,16 @@ extension HomeViewController: CLLocationManagerDelegate {
         }
     }
 
+}
 
+extension HomeViewController: HomeViewDelegate {
+    func didTapMarker() {
+        print("didTapKickboard")
+        let vc = HomeBottomSheetViewController()
+        if let sheet = vc.sheetPresentationController {
+            sheet.detents = [.medium()]
+            sheet.prefersGrabberVisible = true
+        }
+        present(vc, animated: true)
+    }
 }
---
+++ KickboardApp/KickboardApp/PresentationLayer/Onboarding/View/HomeView.swift
@@ -0,0 +1,103 @@
+//
+//  HomeView.swift
+//  KickboardApp
+//
+//  Created by 송규섭 on 4/28/25.
+//
+
+import UIKit
+import NMapsMap
+
+protocol HomeViewDelegate: AnyObject {
+    func didTapMarker()
+}
+
+final class HomeView: UIView {
+    weak var delegate: HomeViewDelegate?
+
+    private let searchTextField = UITextField()
+    private let mapView = NMFMapView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configure()
+    }
+
+    @available(*, unavailable, message: "storyboard is not supported.")
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
+
+}
+
+private extension HomeView {
+    func configure() {
+        setStyle()
+        setHierarchy()
+        setConstraints()
+        setAction()
+    }
+
+    func setStyle() {
+        searchTextField.do {
+            $0.layer.cornerRadius = 10
+            $0.layer.shadowColor = UIColor.black2?.cgColor
+            $0.layer.shadowOpacity = 0.25
+            $0.layer.shadowOffset = CGSize(width: 0, height: 2)
+            $0.layer.shadowRadius = 4
+
+            $0.attributedPlaceholder = NSAttributedString(
+                string: "제주특별자치도 제주시 첨단로 242",
+                attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray3]
+            )
+
+            $0.backgroundColor = UIColor.white
+            $0.leftViewMode = .always
+            $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: self.frame.height))
+
+            var config = UIButton.Configuration.plain()
+            config.baseForegroundColor = UIColor.gray1
+            config.image = UIImage(systemName: "magnifyingglass")
+            config.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 12)
+            let rightButton = UIButton(configuration: config)
+            rightButton.frame = CGRect(x: 0, y: 0, width: 44, height: 48)
+
+            $0.rightViewMode = .always
+            $0.rightView = rightButton
+
+            rightButton.addTarget(self, action: #selector(didTapMarker), for: .touchUpInside)
+        }
+    }
+
+    func setHierarchy() {
+        addSubViews(mapView, searchTextField)
+    }
+
+    func setConstraints() {
+        mapView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        searchTextField.snp.makeConstraints {
+            $0.directionalHorizontalEdges.equalToSuperview().inset(28)
+            $0.top.equalToSuperview().inset(72)
+            $0.height.equalTo(48)
+        }
+    }
+
+    func setAction() {
+
+    }
+
+    @objc private func didTapMarker() {
+        print("didTapMarker")
+        delegate?.didTapMarker()
+    }
+}
+
+extension HomeView {
+    func moveCamera(to update: NMFCameraUpdate) {
+        mapView.moveCamera(update)
+    }
+}
