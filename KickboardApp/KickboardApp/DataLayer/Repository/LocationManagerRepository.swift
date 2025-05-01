374bae4dc28d437dd75c135f0c638d7ce14f3486
LeeSuHyeon <tngus0673@naver.com>
2025-05-01 20:39:17 +0900

    feat/#45: 마커 반경 설정 및 검색 이동 시 마커 표시 구현

--- KickboardApp/KickboardApp/DataLayer/Repository/LocationManagerRepository.swift
+++ KickboardApp/KickboardApp/DataLayer/Repository/LocationManagerRepository.swift
@@ -43,6 +43,7 @@ extension LocationManagerRepository: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let currentLocation = locations.last else { return }
 
+        print(currentLocation.coordinate)
         for case let delegate as LocationManagerRepositoryDelegate in delegates.allObjects {
             delegate.didUpdateLocation(currentLocation)
         }
