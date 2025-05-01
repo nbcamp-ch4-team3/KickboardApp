8e5c89f7953ce3c81333b10f033a8d71935cd961
ChoiAnYong <dksdyd78@naver.com>
2025-05-01 17:09:14 +0900

    feat/#37 - UseInfo 모델 구현

---
+++ KickboardApp/KickboardApp/DomainLayer/Model/UseInfo.swift
@@ -0,0 +1,15 @@
+//
+//  UseInfo.swift
+//  KickboardApp
+//
+//  Created by 최안용 on 5/1/25.
+//
+
+import Foundation
+
+struct UseInfo {
+    let type: String
+    let date: String
+    let address: String
+    let model: String
+}
