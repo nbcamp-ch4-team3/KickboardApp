0000fcfbe63762ce1b1bae341d4b926ee79d8a65
ChoiAnYong <dksdyd78@naver.com>
2025-05-01 17:08:53 +0900

    feat/#37 - History 모델 구현

---
+++ KickboardApp/KickboardApp/DomainLayer/Model/History.swift
@@ -0,0 +1,16 @@
+//
+//  History.swift
+//  KickboardApp
+//
+//  Created by 최안용 on 5/1/25.
+//
+
+import Foundation
+
+struct History {
+    let type: String
+    let date: String
+    let time: String
+    let amount: String
+    let model: String
+}
