01bfaeb1de5de24949fb1568569302b107ad23d6
ChoiAnYong <dksdyd78@naver.com>
2025-05-02 09:14:57 +0900

    feat/#42 - delete 로직 삭제

--- KickboardApp/KickboardApp/DomainLayer/UseCase/DetailViewUseCase.swift
+++ KickboardApp/KickboardApp/DomainLayer/UseCase/DetailViewUseCase.swift
@@ -10,13 +10,12 @@ import Foundation
 protocol DetailViewUseCaseProtocol {
     func getHistory() throws -> [History]
     func getUseInfo() throws -> [UseInfo]
-    func deleteHistory(id: Int) throws
-    func deleteUseInfo(id: Int) throws
 }
 
 final class DetailViewUseCase: DetailViewUseCaseProtocol {
     private let kickboardRepository: KickboardRepositoryProtocol
     private let userRepository: UserRepositoryProtocol
+    private let addressManager = AddressManager()
     
     init(
         kickboardRepository: KickboardRepositoryProtocol,
@@ -28,17 +27,11 @@ final class DetailViewUseCase: DetailViewUseCaseProtocol {
     
     func getHistory() throws -> [History] {
         return [.init(type: "뭐시기", date: "25.03.02", time: "13:32 ~ 15:11", amount: "31,230원", model: "1233A")]
+//        return []
     }
     
     func getUseInfo() throws -> [UseInfo] {
         return [.init(type: "뭐시기", date: "25.03.02", address: "경기도 수원시 팔달구", model: "1233A")]
-    }
-    
-    func deleteHistory(id: Int) throws {
-        
-    }
-    
-    func deleteUseInfo(id: Int) throws {
-        
+//        return []
     }
 }
